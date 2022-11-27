e09b114a40329bccacd59762209e454827ee2308
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-11-27 12:51:51 +0100

    feat(client): add in App GlobalStyle provider, Theme provider and Router provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import GlobalStyle from "./styles/global";
+import { RouteProvider } from "./routes/RouteProvider";
+import { baseTheme } from "./styles/theme";
+import { ThemeProvider } from "styled-components";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App: React.FC = (): any => {
   return (
-    <div className="App">
-      
-    </div>
-  )
-}
-
-export default App
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme = {baseTheme}>
+        <RouteProvider />
+      </ThemeProvider>
+    </>
+  );
+};
